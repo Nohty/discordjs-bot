4fe91fa84b18dcc3b0301b7e83b4e78f68fbe67e
daan-hautekiet <daan.hautekiet@student.howest.be>
2020-10-30 17:13:32 +0100

    added commands [hello, rolldice]

--- bot.js
+++ bot.js
@@ -1,16 +1,22 @@
 require("dotenv").config();
 const discord = require("discord.js");
 const client = new discord.Client();
-
+const PREFIX = process.env.PREFIX;
 client.login(process.env.BOT_TOKEN);
 
 client.on("ready", () => {
 	console.log(`${client.user.tag} has logged in.`);
 });
 
+const isValidCommand = (message, cmdName) =>
+	message.content.toLowerCase().startsWith(PREFIX + cmdName);
+const rollDice = () => Math.floor(Math.random() * 6) + 1;
 client.on("message", (message) => {
 	if (message.author.bot) return;
-	if (message.content === "hello") {
-		message.channel.send("hello");
+	if (isValidCommand(message, "hello")) {
+		message.reply("Hello!");
+	}
+	if (isValidCommand(message, "rolldice")) {
+		message.reply("Rolled a " + rollDice());
 	}
 });
