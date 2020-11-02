657258c46e96efb1e118ba02ebc9433167c5e4d4
daan-hautekiet <daan.hautekiet@student.howest.be>
2020-11-02 22:02:35 +0100

    added checkProperties

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -7,3 +7,12 @@ module.exports.checkCommandModule = (cmdName, cmdModule) => {
         throw new Error(`${cmdNamd} command module does not have property 'aliases'`);
     return true;
 }
+module.exports.checkProperties = (cmdName, cmdModule) => {
+    if(typeof cmdModule.run !== 'function')
+        throw new Error(`${cmdName} command: run is not a function`);
+    if(typeof cmdModule.description !== 'string')
+        throw new Error(`${cmdName} command: description is not a string`);
+    if(!Array.isArray(cmdModule.aliases))
+        throw new Error(`${cmdName} command: aliases is not an Array`);
+    return true;
+}
