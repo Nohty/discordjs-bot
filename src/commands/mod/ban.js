24c2146f53f5149ac8ff4e5f2d2ba6506cefe685
daan-hautekiet <daan.hautekiet@student.howest.be>
2020-11-02 22:13:07 +0100

    added hasPermission ban members

---
+++ src/commands/mod/ban.js
@@ -0,0 +1,7 @@
+module.exports = {
+    run: async(client, message, args) =>{
+        if(!message.member.hasPermission('BAN_MEMBERS?')){
+            message.channel.send("You don't have permission to use that command.");
+        }
+    }
+}
