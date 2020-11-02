80060240c8dede979f1075415410bfdfe5512f42
daan-hautekiet <daan.hautekiet@student.howest.be>
2020-11-02 21:51:44 +0100

    changed bot.js to use an other file for events

--- src/bot.js
+++ src/bot.js
@@ -1,88 +1,12 @@
 require("dotenv").config();
 const discord = require("discord.js");
-const client = new discord.Client();
-const fs = require('fs').promises;
-const path = require('path');
-const PREFIX = process.env.PREFIX;
-client.login(process.env.BOT_TOKEN);
-
-client.on("ready", () => {
-	registerCommands();
-	console.log(`${client.user.tag} has logged in.`);
-});
-
-const isValidCommand = (message, cmdName) =>
-	message.content.toLowerCase().startsWith(PREFIX + cmdName);
-const rollDice = () => Math.floor(Math.random() * 6) + 1;
-
-client.on("message", (message) => {
-	if (message.author.bot) return;
-	if (isValidCommand(message, "hello")) {
-		message.reply("Hello!");
-	} else if (isValidCommand(message, "rolldice")) {
-		message.reply("Rolled a " + rollDice());
-	} else if (isValidCommand(message, "add")) {
-		let args = message.content.toLowerCase().substring(5);
-		let roleNames = args.split(", ");
-		let roleSet = new Set(roleNames);
-		let { cache } = message.guild.roles;
-		roleSet.forEach((roleName) => {
-			let role = cache.find((role) => role.name.toLowerCase() === roleName);
-			if (role) {
-				if (message.member.roles.cache.has(role.id)) {
-					message.channel.send("You already have this role!");
-					return;
-				}
-				if (
-					role.permissions.has("ADMINISTRATOR") ||
-					role.permissions.has("KICK_MEMBERS") ||
-					role.permissions.has("BAN_MEMBERS") ||
-					role.permissions.has("MANAGE_GUILD") ||
-					role.permissions.has("MANAGE_CHANNELS")
-				) {
-					message.channel.send("You cannot add yourself to this role.");
-					return;
-				} else {
-					message.member.roles
-						.add(role)
-						.then((member) =>
-							message.channel.send("You were added to this role!")
-						)
-						.catch((err) => {
-							console.log(err);
-							message.channel.send("Something went wrong...");
-						});
-				}
-			} else {
-				message.channel.send("Role not found!");
-			}
-		});
-	} else if (isValidCommand(message, "del")) {
-		let args = message.content.toLowerCase().substring(5);
-		let roleNames = args.split(", ");
-		let roleSet = new Set(roleNames);
-		let { cache } = message.guild.roles;
-		roleSet.forEach((roleName) => {
-			let role = cache.find((role) => role.name.toLowerCase() === roleName);
-			if (role) {
-				if (message.member.roles.cache.has(role.id)) {
-					message.member.roles
-						.remove(role)
-						.then((member) =>
-							message.channel.send("You were removed to this role!")
-						)
-						.catch((err) => {
-							console.log(err);
-							message.channel.send("Something went wrong...");
-						});
-				}
-			} else {
-				message.channel.send("Role not found!");
-			}
-		});
-	}
-});
-
-async function registerCommands(dir = 'commands'){
-
-}
+const client = new discord.Client({ partials: ['MESSAGE', 'REACTION']});
+const { registerCommands, registerEvents } = require('./utils/registry');
+(async () => {
+    client.login(process.env.BOT_TOKEN);
+    client.commands = new Map();
+    client.cachedMessageReactions = new Map();
+    await registerEvents(client, '../events');
+    await registerCommands(client, '../commands');
+    
+})();
