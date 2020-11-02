2b0d4cd0be3cb30bdedd1ad14d1ca8aeb9e664a9
daan-hautekiet <daan.hautekiet@student.howest.be>
2020-11-02 22:14:55 +0100

    added error handling

--- src/commands/mod/ban.js
+++ src/commands/mod/ban.js
@@ -2,6 +2,12 @@ module.exports = {
     run: async(client, message, args) =>{
         if(!message.member.hasPermission('BAN_MEMBERS?')){
             message.channel.send("You don't have permission to use that command.");
+        } else{
+            try{
+                return;
+            } catch(err){
+                console.log(err);
+            }
         }
     }
 }
