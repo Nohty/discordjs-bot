23002fb16379cb0815f4800b3075911b2aca1d4f
daan-hautekiet <daan.hautekiet@student.howest.be>
2020-11-02 23:21:29 +0100

    fixed bug in args

--- src/events/message/message.js
+++ src/events/message/message.js
@@ -3,7 +3,7 @@ module.exports = (client, message) => {
     if(message.author.bot) return;
     if(!message.content.startsWith(PREFIX)) return;
     let cmdName = message.content.substring(message.content.indexOf(PREFIX)+1).split(new RegExp(/\s+/)).shift();
-    let argsToParse = message.content.substring(message.content.indexOf(' ')+1);
+    let argsToParse = argList = message.content.split(" ").slice(1);
     if(client.commands.get(cmdName))
         client.commands.get(cmdName)(client, message, argsToParse);
     else
