79512ece8778942c35fd849c6e4c2a28c34f3238
daan-hautekiet <daan.hautekiet@student.howest.be>
2020-11-02 22:01:52 +0100

    added the roll command

--- src/commands/fun/roll.js
+++ src/commands/fun/roll.js
@@ -1,3 +1,9 @@
-module.exports.run = async(client, message, args) => {
-    
+const { rollDice } = require('../../utils/dicefn');
+
+module.exports = {
+    run: async(client, message) => {
+        message.reply("rolled a " + rollDice());
+    },
+    aliases: ['dice', 'rolldice'],
+    description: 'Rolls the dice'
 }
