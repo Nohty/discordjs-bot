488827250186c764b32bd72d560b931718671158
daan-hautekiet <daan.hautekiet@student.howest.be>
2020-11-04 13:12:28 +0100

    finised

--- src/commands/mod/ban.js
+++ src/commands/mod/ban.js
@@ -7,7 +7,6 @@ module.exports = {
 
 		function getUser(mention) {
 			if (!mention) return;
-			client.users.fetch()
 			if (mention.startsWith(`<@`) && mention.endsWith(`>`)) {
 				mention = mention.slice(2, -1);
 				if (mention.startsWith(`!`)) {
