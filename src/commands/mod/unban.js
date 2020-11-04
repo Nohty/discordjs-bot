0df3a80688f324f589c92e3aad3006d87b553fe1
daan-hautekiet <daan.hautekiet@student.howest.be>
2020-11-04 11:38:09 +0100

    added hasPermission for bot and user of command

--- src/commands/mod/unban.js
+++ src/commands/mod/unban.js
@@ -1,7 +1,10 @@
 module.exports = {
 	run: async (client, message, args) => {
-		
+		if (!message.member.hasPermission(`BAN_MEMBERS`))
+			return message.reply(`I am sorry, but you can not use this command.`);
+		if (!message.guild.me.hasPermission(`BAN_MEMBERS`))
+			return message.reply(`I am sorry, but I can not ban members.`);
 	},
 	aliases: [],
-	description: "Unbans a guild member by their ID or mention",
+	description: "Unbans a guild member by their ID",
 };
