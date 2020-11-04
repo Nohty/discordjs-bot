720023b6b27d22affefca2b036965c95989ae3d2
daan-hautekiet <daan.hautekiet@student.howest.be>
2020-11-04 13:01:40 +0100

    finished unban command

--- src/commands/mod/unban.js
+++ src/commands/mod/unban.js
@@ -11,14 +11,39 @@ module.exports = {
 				if (mention.startsWith(`!`)) {
 					mention = mention.slice(1);
 				}
-				return client.users.cache.get(mention);
-			} else return client.users.cache.get(mention);
+				return client.users.fetch(mention);
+			} else return client.users.fetch(mention);
 		}
-		let user = getUser(args[0]);
+		let user = await getUser(args[0]);
 		if (!user)
 			return message.channel.send(
 				`Member not found. Please specify a valid User ID or mention the person you would like to ban.`
 			);
+		try {
+			const { MessageEmbed } = require("discord.js");
+			const channel = message.guild.channels.cache.get("767378847636127744");
+			const banList = await message.guild.fetchBans();
+			const bannedUser = banList.find(
+				(bannedMember) => bannedMember.user.id === user.id
+			);
+			if (!bannedUser) return message.channel.send("This user is not banned");
+			message.guild.members.unban(user.id).then(() => {
+				const unbanEmbed = new MessageEmbed()
+					.setDescription(`✅ ***${user.tag} was unbanned***`)
+					.setColor("GREEN");
+				message.channel.send(unbanEmbed);
+				const unbanLogEmbed = new MessageEmbed()
+					.setAuthor("Member Unbanned", user.avatarURL())
+					.setThumbnail(user.avatarURL())
+					.setColor("GREEN")
+					.setTimestamp()
+					.addField("User", `<@${user.id}> ${user.tag}`)
+					.addField("Unbanned By", `<@${message.author.id}>`);
+				channel.send(unbanLogEmbed);
+			});
+		} catch (err) {
+			console.log(err);
+		}
 	},
 	aliases: [],
 	description: "Unbans a guild member by their ID or mention",
