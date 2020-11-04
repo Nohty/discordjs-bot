586626c2fd33ab89ac42c9d4744ae41ff9515bf7
daan-hautekiet <daan.hautekiet@student.howest.be>
2020-11-04 16:20:32 +0100

    added hasPermission for target user

--- src/commands/mod/mute.js
+++ src/commands/mod/mute.js
@@ -25,6 +25,10 @@ module.exports = {
 			return message.channel.send(
 				`Member not found. Please specify a valid User ID or mention the person you would like to ban.`
 			);
+		if (member.hasPermission(`KICK_MEMBERS`))
+			return message.channel.send(
+				"❌ That user is a mod/admin, I can't do that."
+			);
 	},
 	aliases: [],
 	description: "Mutes a user",
