81506478a0ca704b8a9d713f5bd7f4781784c0c9
daan-hautekiet <daan.hautekiet@student.howest.be>
2020-11-04 17:02:29 +0100

    added perm check

--- src/commands/mod/unmute.js
+++ src/commands/mod/unmute.js
@@ -1,7 +1,10 @@
 module.exports = {
-    run: async(client, message, args) => {
-        
-    },
-    aliases: [],
-    description: 'Unmutes a user'
-}
+	run: async (client, message, args) => {
+		if (!message.member.hasPermission(`KICK_MEMBERS`))
+			return message.reply(`I am sorry, but you can not use this command.`);
+		if (!message.guild.me.hasPermission(`KICK_MEMBERS`))
+			return message.reply(`I am sorry, but I can not unmute members.`);
+	},
+	aliases: [],
+	description: "Unmutes a user",
+};
