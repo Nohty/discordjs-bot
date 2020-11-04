d39202a15709cf792d63ab7c43ba4204ea2af0d7
daan-hautekiet <daan.hautekiet@student.howest.be>
2020-11-04 11:42:06 +0100

    added function getUser

--- src/commands/mod/unban.js
+++ src/commands/mod/unban.js
@@ -4,6 +4,16 @@ module.exports = {
 			return message.reply(`I am sorry, but you can not use this command.`);
 		if (!message.guild.me.hasPermission(`BAN_MEMBERS`))
 			return message.reply(`I am sorry, but I can not ban members.`);
+		function getUser(mention) {
+			if (!mention) return;
+			if (mention.startsWith(`<@`) && mention.endsWith(`>`)) {
+				mention = mention.slice(2, -1);
+				if (mention.startsWith(`!`)) {
+					mention = mention.slice(1);
+				}
+				return client.users.cache.get(mention);
+			} else return client.users.cache.get(mention);
+		}
 	},
 	aliases: [],
 	description: "Unbans a guild member by their ID",
