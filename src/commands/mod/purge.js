94760df0e076c7b69f72b5d3c052eb6dc4f0679f
daan-hautekiet <daan.hautekiet@student.howest.be>
2020-12-20 11:51:16 +0100

    created the purge command

---
+++ src/commands/mod/purge.js
@@ -0,0 +1,26 @@
+module.exports = {
+  run: async (client, message, args) => {
+    if (!message.member.hasPermission(`MANAGE_MESSAGES`))
+      return message.reply(`I am sorry, but you can not use this command.`);
+    if (!message.guild.me.hasPermission(`MANAGE_MESSAGES`))
+      return message.reply(`I am sorry, but I can not remove messages.`);
+    let amount = 5;
+    try {
+      if (args[0]) amount = args[0];
+    } catch (err) {
+      console.log(err);
+      return message.reply("An error occurred. make sure to use a number.");
+    }
+    if (amount > 500)
+      return message.reply("You cannot remove more than 500 messages");
+    if (amount < 1)
+      return message.reply("You cannot remove less than 1 message.");
+    try {
+      message.channel.bulkDelete(parseInt(amount + 1));
+    } catch (err) {
+      return message.reply("An error occurred when removing the messages");
+    }
+  },
+  aliases: [],
+  description: "removes multiple messages from a channel (deafult 5)",
+};
