0a34966d43bbd36643259d98480f758ccbbdfd9f
daan-hautekiet <daan.hautekiet@student.howest.be>
2020-12-20 11:31:43 +0100

    added get prefix when bot is tagged

--- src/events/message/message.js
+++ src/events/message/message.js
@@ -1,11 +1,19 @@
 const PREFIX = process.env.PREFIX;
 module.exports = (client, message) => {
-    if(message.author.bot) return;
-    if(!message.content.startsWith(PREFIX)) return;
-    let cmdName = message.content.substring(message.content.indexOf(PREFIX)+1).split(new RegExp(/\s+/)).shift();
-    let argsToParse = argList = message.content.split(" ").slice(1);
-    if(client.commands.get(cmdName))
-        client.commands.get(cmdName)(client, message, argsToParse);
-    else
-        console.log("Command does not exist.");
+  if (message.author.bot) return;
+  if (
+    (message.content === `<@!${client.user.id}>`) |
+    (message.content === `<@${client.user.id}>`)
+  ) {
+    return message.channel.send(`My prefix is \`${PREFIX}\``);
+  }
+  if (!message.content.startsWith(PREFIX)) return;
+  let cmdName = message.content
+    .substring(message.content.indexOf(PREFIX) + 1)
+    .split(new RegExp(/\s+/))
+    .shift();
+  let argsToParse = (argList = message.content.split(" ").slice(1));
+  if (client.commands.get(cmdName))
+    client.commands.get(cmdName)(client, message, argsToParse);
+  else console.log("Command does not exist.");
 };
