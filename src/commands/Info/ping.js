7ea56b9fcd61d606a1b69115536c1d01d12250ce
daan-hautekiet <daan.hautekiet@student.howest.be>
2020-12-20 11:31:23 +0100

    fixed ping description

--- src/commands/Info/ping.js
+++ src/commands/Info/ping.js
@@ -1,12 +1,12 @@
 module.exports = {
-	run: async (client, message, args) => {
-		const m = await message.channel.send("Ping?");
-		m.edit(
-			`Pong! Latency is ${
-				m.createdTimestamp - message.createdTimestamp
-			}ms. API Latency is ${Math.round(client.ws.ping)}ms`
-		);
-	},
-	aliases: [],
-	description: "Unbans a guild member by their ID or mention",
+  run: async (client, message, args) => {
+    const m = await message.channel.send("Ping?");
+    m.edit(
+      `Pong! Latency is ${
+        m.createdTimestamp - message.createdTimestamp
+      }ms. API Latency is ${Math.round(client.ws.ping)}ms`
+    );
+  },
+  aliases: [],
+  description: "Displays the Latency.",
 };
