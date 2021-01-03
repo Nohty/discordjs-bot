1a732293779c35ff3a5017d3c229eb6b11d458b0
daan-hautekiet <daan.hautekiet@student.howest.be>
2021-01-03 14:13:08 +0100

    added teamplate for help and role command

---
+++ src/commands/help/help.js
@@ -0,0 +1,7 @@
+module.exports = {
+  run: async (client, message, args) => {
+    message.channel.send("No");
+  },
+  aliases: [],
+  description: "Displays help about the bot and its commands",
+};
---
+++ src/commands/roles/role.js
@@ -0,0 +1,7 @@
+module.exports = {
+  run: async (client, message, args) => {
+    message.channel.send("🚨 Command not ready");
+  },
+  aliases: [],
+  description: "To check, Add and Remove Roles",
+};
